e49ddb4f52d1aed8943632d5f6ef9a8f122e5109
aoi0108 <hira.aoi.18@gmail.com>
2025-12-03 15:30:59 +0900

    custom layout view

--- TutorialApp/Tabs/Moments/MomentsView.swift
+++ TutorialApp/Tabs/Moments/MomentsView.swift
@@ -1,1 +1,61 @@
+import SwiftUI
+import SwiftData
 
+struct MomentsView: View {
+    @State private var showCreateMoment = false
+    @Query(sort:\Moment.timestamp)
+    private var moments: [Moment]
+    
+    var body: some View {
+        NavigationStack{
+            ScrollView{
+                pathItems
+                    .frame(maxWidth: .infinity)
+            }
+            .overlay {
+                if moments.isEmpty {
+                    ContentUnavailableView {
+                        Label("No moments yet!", systemImage: "exclamationmark.circle")
+                    } description: {
+                        Text("Post a note or photo to start filling this space with gratitude.")
+                    }
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .primaryAction){
+                    Button {
+                        showCreateMoment = true
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+                    .sheet(isPresented: $showCreateMoment){
+                        MomentEntryView()
+                    }
+                }
+            }
+            .navigationTitle("Grateful Moments")
+        }
+    }
+    
+    private var pathItems: some View{
+        ForEach(moments){ moment in
+            NavigationLink {
+                MomentDetailView(moment: moment)
+            } label :{
+                Text(moment.title)
+            }
+        }
+    }
+}
+
+
+#Preview {
+    MomentsView()
+        .sampleDataContainer()
+}
+
+
+#Preview ("No moments") {
+    MomentsView()
+        .modelContainer(for: [Moment.self])
+}
